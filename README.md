63675f222afa5dac5715a5fe87c07723980ac984
yoqu <yoqulin@qq.com>
2019-10-12 19:22:16 +0800

    撤销单独兼容linux修改

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 实现实时Markdown转Html的一款小工具
 
+
+界面展示：
+
+![UTOOLS1570873258810.png](https://i.loli.net/2019/10/12/QjHte718s9JuqKr.png)
+
 使用框架
 
 * [marked](https://github.com/markedjs/marked)
--- js/preload.js
+++ js/preload.js
@@ -1,24 +1,11 @@
-const { clipboard } = require('electron')
-const os = require('os');
-writeHtml= function(html){
-    if(os.type()=="Linux"){//linux系统
-        clipboard.writeHTML(html,"selection")
-    }else{
-        clipboard.writeHTML(html)
-    }
-}
+const { clipboard } = require("electron");
+writeHtml = function(html) {
+  clipboard.writeHTML(html);
+};
 
-writeText = function(text){
-    if(os.type()=="Linux"){//linux系统
-        clipboard.writeText(text,"selection")
-    }else{
-        clipboard.writeText(text)
-    }
-}
-readText = function(text){
-    if(os.type()=="Linux"){//linux系统
-        return clipboard.readText("selection")
-    }else{
-        return clipboard.readText()
-    }
-}
+writeText = function(text) {
+  clipboard.writeText(text);
+};
+readText = function(text) {
+  return clipboard.readText();
+};
